c75d2975ab4e1fcecf83f2bc81c0f6c1f8e3566e
freezy <marek.viger@gmail.com>
2020-07-12 13:30:12 +0200

    updated init scripts

---
+++ init/brew-cask.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Install GUI Applications using Homebrew Cask
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed cask.
+brew cask upgrade
+
+# Install applications
+brew cask install 1password
+brew cask install atom
+brew cask install caffeine
+brew cask install coconutbattery
+brew cask install cyberduck
+brew cask install docker
+brew cask install dropbox
+brew cask install handbrake
+brew cask install iterm2
+brew cask install jetbrains-toolbox
+brew cask install onyx
+brew cask install osxfuse
+brew cask install scroll-reverser
+brew cask install skyfonts
+brew cask install spotify
+brew cask install tunnelblick
+brew cask install vlc
---
+++ init/mas.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Install Mac App Store applications using mas-cli
+# ================================================
+
+# Install CLI for the Mac App Store if not installed
+if ! [ -x "$(command -v mas)" ]; then
+    brew install mas
+fi
+
+# Xcode
+mas install 497799835
+
+# Magnet
+mas install 441258766
+
+# Moom
+mas install 419330170
+
+# Slack
+mas install 803453959
+
+# Surfshark VPN
+mas install 1437809329
+
